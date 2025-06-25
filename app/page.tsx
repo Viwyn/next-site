e22810ead4ae53b5592aaf637f5754347cf84391
Viwyn <riaruplays@gmail.com>
2025-06-25 22:54:22 +0800

    added text

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,11 @@ export default async function MainPage() {
         <div className="min-h-screen min-w-full flex flex-col bg-gray-100 dark:bg-black">
             <Header></Header>
             <main className="flex flex-col justify-center items-center flex-grow">
-                <div className="border-4 rounded-xl border-black dark:border-white h-[550px] w-[850px] justify-center items-center flex bg-gray-400 dark:bg-gray-800 shadow-xl/40 dark:shadow-gray-600">
+                <div className="border-4 rounded-xl border-black dark:border-white h-[550px] w-[850px] justify-center items-center flex flex-col bg-gray-400 dark:bg-gray-800 shadow-xl/40 dark:shadow-gray-600">
                     <p className="text-4xl text-black dark:text-white">
                         Welcome to <span className="font-bold italic text-pink-300 text-shadow-lg text-bold">FaylieLabs</span>.
                     </p>
+                    <p className="mt-10 text-xl font-extralight dark:text-white text-black">More to come in the future~</p>
                 </div>
             </main>
             <Footer></Footer>

74ed9fca6b10fc7d2708338be7677b9ce32169f5
Viwyn <riaruplays@gmail.com>
2025-07-03 13:32:54 +0800

    added buttons, titles, custom icons for floating divs

--- app/components/DraggableDiv.tsx
+++ app/components/DraggableDiv.tsx
@@ -1,24 +1,27 @@
 'use client'
 
 import React, { useState, useRef, useEffect, ReactNode } from "react"
+import { IconType } from "react-icons";
 
 interface DraggableFloatingDivProps {
     children?: ReactNode;
     initialX?: number;
     initialY?: number;
     isVisible?: boolean;
+    icon?: ReactNode;
+    title?: string;
 }
 
-const DraggableFloatingDiv: React.FC<DraggableFloatingDivProps> = ({ children, initialX = 50, initialY = 50, isVisible}) => {
+const DraggableFloatingDiv: React.FC<DraggableFloatingDivProps> = ({ children, initialX = 50, initialY = 50, isVisible, icon, title}) => {
     const [position, setPosition] = useState({ x: initialX, y: initialY })
     const [isDragging, setIsDragging] = useState(false)
     const [initialMousePosition, setInitialMousePosition] = useState({ x: 0, y: 0 })
-    const [isHidden, setIsHidden] = useState(false)
+    const [isHidden, setIsHidden] = useState(isVisible)
 
     const divRef = useRef<HTMLDivElement>(null)
 
     const handleMouseDown = (e: React.MouseEvent) => {
-        if (!isVisible) return //do nothing if not visible
+        if (!isHidden) return //do nothing if not visible
 
         e.preventDefault()
         setIsDragging(true)
@@ -62,14 +65,20 @@ const DraggableFloatingDiv: React.FC<DraggableFloatingDivProps> = ({ children, i
     }, [isDragging, initialMousePosition])
 
     return (
-        <div ref={divRef} className={`${isDragging ? 'cursor-grabbing' : 'cursor-grab'} fixed z-99 bg-gray-700 text-black dark:text-white rounded-lg border-2 border-black dark:border:white shadow-xl select-none transition-colors flex flex-col ${isHidden ? 'invisible' : ''}`} style={{ left: position.x, top: position.y }} onMouseDown={handleMouseDown}>
-            <div className="flex justify-end items-center">
-                <button onClick={toggleInvisibility} className="py-1 px-3 cursor-pointer text-white text-xl hover:scale-110 transition-transform duration-150">
-                    X
+        <div>
+            <button onClick={toggleInvisibility} className="bg-gray-700 text-black dark:text-white p-4 rounded-full shadow-xl transition-transform duration-150 hover:scale-110 cursor-pointer">
+                {icon}
                 </button>
-            </div>
-            <div>
-                {children}
+            <div ref={divRef} className={`${isDragging ? 'cursor-grabbing' : 'cursor-default'} fixed z-99 bg-gray-700 text-black dark:text-white rounded-lg border-2 border-black dark:border:white shadow-xl select-none transition-all duration-150 flex flex-col ${!isHidden ? 'opacity-0 scale-0 pointer-events-none z-0' : 'opacity-100 scale-100 pointer-events-auto z-10'} max-w-1/2`} style={{ left: position.x, top: position.y }} onMouseDown={handleMouseDown}>
+                <div className="flex justify-between items-center">
+                    <p className="ml-3">{title}</p>
+                    <button onClick={toggleInvisibility} className="py-1 px-3 cursor-pointer text-white text-xl hover:scale-110 transition-transform duration-150">
+                        X
+                    </button>
+                </div>
+                <div>
+                    {children}
+                </div>
             </div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -2,39 +2,62 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import DraggableFloatingDiv from "./components/DraggableDiv";
 import { useState } from "react";
+import { FaLink, FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 export default function MainPage() {
-    return (
-        <div className="min-h-screen min-w-full flex flex-col bg-gray-100 dark:bg-black">
-            <Header></Header>
-            <main className="flex flex-col justify-center items-center flex-grow">
-                <div className="border-4 rounded-xl border-black dark:border-white h-[550px] w-[850px] justify-center items-center flex flex-col bg-gray-400 dark:bg-gray-800 shadow-xl/40 dark:shadow-gray-600">
-                    <p className="text-4xl text-black dark:text-white">
-                        Welcome to <span className="font-bold italic text-pink-300 text-shadow-lg text-bold">FaylieLabs</span>.
-                    </p>
-                    <p className="mt-10 text-xl font-extralight dark:text-white text-black">More to come in the future~</p>
-                </div>
+	return (
+		<div className="min-h-screen min-w-full flex flex-col bg-gray-100 dark:bg-black">
+			<Header></Header>
+			<main className="flex flex-col justify-center items-center flex-grow">
+				<div className="border-2 rounded-xl border-black dark:border-white h-[450px] w-[750px] justify-center items-center flex flex-col bg-gray-400 dark:bg-gray-800 shadow-xl/40 dark:shadow-gray-600">
+					<p className="text-4xl text-black dark:text-white">
+						Welcome to{" "}
+						<span className="font-bold italic text-pink-300 text-shadow-lg text-bold">
+							FaylieLabs
+						</span>
+						.
+					</p>
+					<p className="mt-10 text-xl font-extralight dark:text-white text-black">
+						Page is a work in progress~
+					</p>
 
-                {/* buttons for floating divs */}
-                <div className="mt-4 flex gap-4"> {/* Added a div to contain the buttons */}
-
-                </div>
-
-                {/* div 1 */}
-                <DraggableFloatingDiv initialX={50} initialY={50} isVisible={true}>
-                    <div className="w-full h-full flex flex-col justify-center items-center bg-purple-500 p-5 rounded-lg">
-                        <h1 className="text-2xl font-bold text-black dark:text-white">Drag me around!</h1>
-                        <p className="text-lg text-gray-700 dark:text-gray-300 mt-2">I'm a draggable floating div.</p>
+                    {/* buttons for floating divs */}
+                    <div className="mt-4 flex gap-4">
+                        <DraggableFloatingDiv initialX={50} initialY={50} isVisible={false} icon={<FaUser size={24} />} title="About Me">
+                            <div className="w-full h-full flex flex-col justify-center items-center bg-purple-500 p-5 rounded-lg">
+                                <h1 className="text-2xl font-bold text-black dark:text-white">
+                                    Drag me around!
+                                </h1>
+                                <p className="text-lg text-gray-700 dark:text-gray-300 mt-2">
+                                    I'm a draggable floating div.
+                                </p>
+                            </div>
+                        </DraggableFloatingDiv>
+                        <DraggableFloatingDiv initialX={50} initialY={50} isVisible={false} icon={<FaLink size={24} />} title="Links">
+                            <div className="w-full h-full flex flex-col justify-center items-center bg-purple-500 p-5 rounded-lg">
+                                <h1 className="text-2xl font-bold text-black dark:text-white">
+                                    Drag me around!
+                                </h1>
+                                <p className="text-lg text-gray-700 dark:text-gray-300 mt-2">
+                                    I'm a draggable floating div.
+                                </p>
+                            </div>
+                        </DraggableFloatingDiv>
+                        <DraggableFloatingDiv initialX={50} initialY={50} isVisible={false} icon={<MdEmail size={24} />} title="Contact">
+                            <div className="w-full h-full flex flex-col justify-center items-center bg-purple-500 p-5 rounded-lg">
+                                <h1 className="text-2xl font-bold text-black dark:text-white">
+                                    Drag me around!
+                                </h1>
+                                <p className="text-lg text-gray-700 dark:text-gray-300 mt-2">
+                                    I'm a draggable floating div.
+                                </p>
+                            </div>
+                        </DraggableFloatingDiv>
                     </div>
-                </DraggableFloatingDiv>
-                <DraggableFloatingDiv initialX={50} initialY={50} isVisible={true}>
-                    <div className="w-full h-full flex flex-col justify-center items-center bg-purple-500 p-5 rounded-lg">
-                        <h1 className="text-2xl font-bold text-black dark:text-white">Drag me around!</h1>
-                        <p className="text-lg text-gray-700 dark:text-gray-300 mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit architecto obcaecati impedit, saepe quia, optio suscipit harum animi, voluptate ullam alias esse laborum at facere doloribus laudantium quas magnam omnis?</p>
-                    </div>
-                </DraggableFloatingDiv>
-            </main>
-            <Footer></Footer>
-        </div>
-    );
+				</div>
+			</main>
+			<Footer></Footer>
+		</div>
+	);
 }
